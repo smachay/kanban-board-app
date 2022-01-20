5abd9b7005283c2417daeffd3fbbf78fe25e2afb
smachay <s.machay10@gmail.com>
2022-01-20 20:19:32 +0100

    ehh

---
+++ client/src/components/Projects/AddMilestoneForm.js
@@ -0,0 +1,58 @@
+import {
+  Dialog,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  Typography,
+  TextField,
+  Select,
+  InputLabel,
+  MenuItem,
+} from "@mui/material";
+import React, { useEffect, useState } from "react";
+
+export const AddMilestoneForm = (props) => {
+  const [name, setName] = useState(" ");
+
+  const handleClose = () => {
+    props.close(name);
+  };
+
+  const addMilestone = () => {
+    //passing new milestone name to the parent component
+    handleClose();
+  };
+
+  return (
+    <Dialog open={props.open}>
+      <DialogContent
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          minWidth: 330,
+          p: 4,
+        }}
+      >
+        <Typography>
+          <b>Podaj nazwę nowego zespołu</b>
+        </Typography>
+        <TextField
+          onChange={(event) => setName(event.target.value)}
+          fullWidth
+        />
+      </DialogContent>
+      <DialogActions
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Button onClick={addMilestone}>Dodaj nowy kamień milowy</Button>
+        <Button onClick={handleClose}>Anuluj</Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -1,43 +1,104 @@
-import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
-import React from 'react'
+import {
+  Button,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from "@mui/material";
+import React, { useState } from "react";
+import { BrowserRouter, Routes } from "react-router-dom";
+import ListOfMilestones from "../Lists/ListOfMilestones";
 
 function createData(id, name) {
-    return { id, name};
-  }
-  
-  const projects = [
-        createData(1,"Aplikacja dla wojska"),
-        createData(2,"Aplikacja mobilna dla firmy ogrodniczej"),
-        createData(3,"Aplikacj To-Do")
-  ];
-
-const Projects = () => {
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell><b>Nazwa projektu</b></TableCell>
-                        <TableCell align="left"><b></b></TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {projects.map((row) => (
-                        <TableRow
-                            key={row.id}
-                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-                        >
-                            <TableCell component="th" scope="row">
-                                {row.name}
-                            </TableCell>
-                            <TableCell align="left"><Button>Wyświetl kamienie milowe</Button></TableCell>
-                            
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </TableContainer>
-    )
+  return { id, name };
 }
 
-export default Projects
+const projects = [
+  createData(1, "Aplikacja dla wojska"),
+  createData(2, "Aplikacja mobilna dla firmy ogrodniczej"),
+  createData(3, "Aplikacj To-Do"),
+];
+const milestones = [
+  {
+    milestoneId: 1,
+    name: "Dodanie przycisków na stronie głównej",
+    teamName: "zespół 1",
+    projectId: 1,
+  },
+  {
+    milestoneId: 2,
+    name: "Strona klienta",
+    teamName: null,
+    projectId: 1,
+  },
+];
+
+const Projects = (props) => {
+  const [openMilestones, setOpenMilestones] = useState(false);
+  const [projectId, setProjectId] = useState(null);
+
+  const showMilestones = (projectId) => {
+    setProjectId(projectId);
+    setOpenMilestones(!openMilestones);
+  };
+
+  const addMilestone = (name) => {
+    milestones.push({
+      milestoneId: null,
+      name: name,
+      teamName: null,
+      projectId: projectId,
+    });
+    console.log(milestones);
+  };
+
+  return (
+    <div>
+      {openMilestones === true ? (
+        <ListOfMilestones
+          update={addMilestone}
+          user={props.user}
+          view={2}
+          milestones={milestones}
+        />
+      ) : (
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>
+                  <b>Nazwa projektu</b>
+                </TableCell>
+                <TableCell align="left">
+                  <b></b>
+                </TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {projects.map((row) => (
+                <TableRow
+                  key={row.id}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.name}
+                  </TableCell>
+                  <TableCell align="left">
+                    <Button onClick={showMilestones.bind(this, row.id)}>
+                      Wyświetl kamienie milowe
+                    </Button>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      )}
+    </div>
+  );
+};
+
+export default Projects;
