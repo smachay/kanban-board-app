bc70b1c463879b3f4e5167b7545ba4f26e10b0ab
taydexx <jakub.tadeusz@interia.pl>
2022-01-20 14:30:04 +0100

    Add connection to team API

--- client/src/components/Teams/Team.js
+++ client/src/components/Teams/Team.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ListOfEmployees from "../ListOfEmployees/ListOfEmployees";
 
 function createData(id, firstName, lastName, email, jobTitle) {
   return { id, firstName, lastName, email, jobTitle };
 }
 
-const team = [
-  createData(1, "Marcin", "Świderski", "m.swiderski@gmail.com", "Programista"),
-  createData(2, "Karol", "Bagins", "k.bagins@gmail.com", "Programista"),
-  createData(3, "Stefan", "Machay", "s.machay@gmail.com", "Tester"),
-];
+
 
 const Team = (props) => {
+
+
+  const [team, setTeam] = useState([
+    // createData(1, "Marcin", "Świderski", "m.swiderski@gmail.com", "Programista"),
+    // createData(2, "Karol", "Bagins", "k.bagins@gmail.com", "Programista"),
+    // createData(3, "Stefan", "Machay", "s.machay@gmail.com", "Tester"),
+   ]);
+
+   useEffect(() => {
+    fetch('http://localhost:3001/employees/' + props.user.id + "/team", {
+      method: 'get',
+      headers: {'Content-Type':'application/json'}
+   }).then(resp=>resp.json()).then(
+     x=>{
+       let t = [];
+       x.forEach(user => {
+         console.log(user);
+      t.push(createData(user.id, user.first_name, user.last_name, user.email, user.job_title))
+    })
+    setTeam(t);
+  }
+    )
+  }, []);
   /*
     get team members from db
     */
